2f734fd292183ed617ab7720d282394fcc233776
Brandon King <brandonscottking@gmail.com>
2021-02-28 22:02:59 -0500

    added event listener to fetch api data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-//api key for weather e488a40323177664f2fc7533181baeb8
 //declare variables
+let apiKey = 'e488a40323177664f2fc7533181baeb8'
 let historyEl = document.querySelector('#search-history');
 let currentEl = document.querySelector('#current-weather');
 let forecastEl = document.querySelector('#forecast');
@@ -7,6 +7,14 @@ let searchInput = document.querySelector('input');
 let searchBtn = document.querySelector('button');
 
 //search click event
-searchBtn.addEventListener('click', function(){
-    //put code here
-})
+searchBtn.addEventListener('click', function(event){
+    event.preventDefault();
+    let userCity = searchInput.value;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${apiKey}`;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => console.log(data))
+    console.log(userCity)
+});
+
+
