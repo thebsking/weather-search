ac45e1289146627f830736db151de4c36e88f398
Brandon King <brandonscottking@gmail.com>
2021-02-28 21:42:02 -0500

    declared variables in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//api key for weather e488a40323177664f2fc7533181baeb8
+//declare variables
+let historyEl = document.querySelector('#search-history');
+let currentEl = document.querySelector('#current-weather');
+let forecastEl = document.querySelector('#forecast');
+let searchInput = document.querySelector('input');
+let searchBtn = document.querySelector('button');
+
+//search click event
+searchBtn.addEventListener('click', function(){
+    //put code here
+})
