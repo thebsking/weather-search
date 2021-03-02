bff1f69a9a7897fd30dbb2dba8875299530d4e92
Brandon King <brandonscottking@gmail.com>
2021-03-02 17:32:35 -0500

    added script to put cities into 'search history'

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,10 @@ searchBtn.addEventListener('click', function(event){
     event.preventDefault();
     let userCity = searchInput.value;
     //add search to history list
-    
+    let historyItem = document.createElement('button')
+    historyItem.textContent = userCity
+    historyItem.classList.add('historyBtn')
+    historyEl.appendChild(historyItem)
     //fetch weather data
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${apiKey}&units=imperial`;
     fetch(apiUrl)
