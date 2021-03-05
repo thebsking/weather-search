9bc9969415b5f4126979404fc50108b425596341
Brandon King <brandonscottking@gmail.com>
2021-03-04 22:52:42 -0500

    added search history to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ let currentEl = document.querySelector('#current-weather');
 let forecastEl = document.querySelector('#forecast');
 let searchInput = document.querySelector('input');
 let searchBtn = document.querySelector('button');
+let today = moment().format('MM/DD/yyyy');
+let index = 0;
 let userCity;
 let firstData;
 let secondData;
@@ -18,7 +20,6 @@ function firstApiCall() {
             firstData = data;
             secondApiCall(firstData.coord.lat, firstData.coord.lon);
         })
-    
 };
 
 function secondApiCall(lati, long){
@@ -28,16 +29,40 @@ function secondApiCall(lati, long){
             secondData = data;
             console.log(secondData)
         })
+    showData();
+}
+
+function showData() {
+    let showCity = document.createElement('h2');
+    showCity.textContent = firstData.name + ' (' + today + ')';
+    currentEl.appendChild(showCity)
 }
 
 //search click event
 searchBtn.addEventListener('click', function(event){
     event.preventDefault();
-   firstApiCall();
+    currentEl.innerHTML = ''
+    firstApiCall();
+    
     //add search to history list
     let historyItem = document.createElement('button')
     historyItem.textContent = userCity
     historyItem.classList.add('historyBtn')
+    historyItem.setAttribute('id', `item-${index}`)
+    index++;
     historyEl.appendChild(historyItem);
+    window.localStorage.setItem(historyItem.getAttribute('id'), userCity)
+    searchInput.value = '';
 })
 
+//display search history on reload
+if (window.localStorage.length > 0) {
+    for(var i = 0; i < window.localStorage.length; i++) {
+        let showCity = window.localStorage.getItem(`item-${i}`)
+        let historyItem = document.createElement('button')
+        historyItem.textContent = showCity
+        historyItem.classList.add('historyBtn')
+        historyItem.setAttribute('id', `item-${i}`)
+        historyEl.appendChild(historyItem);
+    }
+}
